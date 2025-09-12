f03dc4b9c3b5b38e40bd4e4d05591ed5a31a19f1
mzmxaming <55690144+mzmxaming@users.noreply.github.com>
2025-09-12 22:03:19 +0800

    Update MainActivity.kt

--- app/src/main/java/com/app/pakeplus/MainActivity.kt
+++ app/src/main/java/com/app/pakeplus/MainActivity.kt
@@ -115,8 +115,8 @@ class MainActivity : AppCompatActivity() {
             false
         }
 
-        // webView.loadUrl("https://juejin.cn/")
-        webView.loadUrl("file:///android_asset/index.html")
+          webView.loadUrl("http://www.mxcgm.top:9999/ai/gwen.html")
+        //webView.loadUrl("file:///android_asset/index.html")
 
 //        binding = ActivityMainBinding.inflate(layoutInflater)
 //        setContentView(R.layout.single_main)
